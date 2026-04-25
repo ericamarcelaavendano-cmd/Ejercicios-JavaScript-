1f39f2988d532cff733f3d517dad3ceb1a4c65cb
Erica Avendaño <ericamarcelaavendano@gmail.com>
2026-04-25 10:18:26 -0500

    Recto de contador

---
+++ contador.js
@@ -0,0 +1,32 @@
+
+ //agregar dos botones mas uno que disminuya  y el otro para reiniciar 
+
+
+let cuenta = 0;
+function aumentar() {
+    cuenta++; 
+    document.getElementById("numero").innerHTML = cuenta;
+    //Aqui cambio el color de aumentar por verde
+    document.getElementById("numero").style.color = "green";
+}
+
+// FUNCIÓN PARA DISMINUIR 
+function disminuir() {
+    cuenta--; 
+    document.getElementById("numero").innerHTML = cuenta;
+    //si pasa menor de cero empieza con color rojo
+    if (cuenta < 0) {
+        //Aqui cambiio el color tambien 
+        document.getElementById("numero").style.color = "red";
+    }
+}
+
+// FUNCIÓN PARA REINICIAR 
+function reiniciar() {
+    cuenta = 0; 
+    document.getElementById("numero").innerHTML = cuenta;
+    // Para que pase a otro color 
+    document.getElementById("numero").style.color = "black";
+}
+
+
